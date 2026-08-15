f54f2f2244ea7c754214e3e6d68472f3a361c679
Nikolai <155324273+lga1156@users.noreply.github.com>
2026-08-15 12:47:37 +0300

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        const freq = new Map();
+        for (const num of nums) {
+            freq.set(num, (freq.get(num) || 0) + 1);
+        }
+        for (const [key, ] of freq) {
+            if (freq.get(key) > 1) {
+                return true
+            }
+        }
+        return false
+    }
+}
